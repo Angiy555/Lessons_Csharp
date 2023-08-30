71cfc710d820866c30413ad046464e57e1fcb775
HOME\Дом <angiy.kaa555@yandex.ru>
2023-08-30 15:21:47 +0300

    Adding new commit file Ex_4

--- Ex_4/Program.cs
+++ Ex_4/Program.cs
@@ -1,5 +1,9 @@
 ﻿Console.Clear();
-int xa = 1, ya = 1, xb = 1, yb = 30, xc = 80, yc = 30;
+/*Console.SetCursorPosition(10, 1);
+Console.WriteLine("+");*/
+int xa = 30, ya = 1, 
+xb = 1, yb = 10,
+ xc = 40, yc = 20;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 
@@ -9,23 +13,23 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-int x = xa, y = ya;
+int x = xa, y = xb;
 int count = 0;
 
-while(count < 10000)
+while (count < 10000)
 {
     int what = new Random().Next(0, 3);
-    if(what == 0)
+    if (what == 0)
     {
         x = (x + xa)/2;
         y = (y + ya)/2;
     }
-    if(what == 1)
+    if (what == 1)
     {
         x = (x + xb)/2;
         y = (y + yb)/2;
     }
-    if(what == 2)
+    if (what == 2)
     {
         x = (x + xc)/2;
         y = (y + yc)/2;
