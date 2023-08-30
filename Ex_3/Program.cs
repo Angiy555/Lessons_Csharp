b8314230eced98364adc416d90d7d6eed0215340
HOME\Дом <angiy.kaa555@yandex.ru>
2023-08-30 10:01:07 +0300

    Adding file Ex_3

---
+++ Ex_3/Program.cs
@@ -0,0 +1,6 @@
+﻿double num_1 = new Random().Next(1, 10);
+double num_2 = new Random().Next(1, 5);
+Console.WriteLine(num_1);
+Console.WriteLine(num_2);
+double sum = num_1 + num_2;
+Console.WriteLine("Сумма = " + sum);
