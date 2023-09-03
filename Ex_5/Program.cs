bb0c8e3c7d1ccf142fa3d448d452113cfbd1f9a3
HOME\Дом <angiy.kaa555@yandex.ru>
2023-09-03 19:04:33 +0300

    Adding file Ex_5

---
+++ Ex_5/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {11,25,34,4,25,67,78,8};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n) 
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+}
