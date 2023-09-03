acac1543f20b6f2bc26bf9bc202bd0fdf2805d18
HOME\Дом <angiy.kaa555@yandex.ru>
2023-09-03 19:26:38 +0300

    Adding new Ex_5

--- Ex_5/Program.cs
+++ Ex_5/Program.cs
@@ -1,15 +1,43 @@
-﻿int [] array = {11,25,34,4,25,67,78,8};
-
-int n = array.Length;
-int find = 4;
-
-int index = 0;
-
-while (index < n) 
+﻿void FillArray(int[] collection)
 {
-    if (array[index] == find)
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
     {
-        Console.WriteLine(index);
-        break;
+        collection[index] = new Random().Next(1, 10);
+        index++;
     }
 }
+void PrintArray(int[] col)
+{
+    int  count = col.Length;
+    int pos = 0;
+    while(pos < count)
+    {
+        Console.WriteLine(col[pos]);
+        pos++;
+    }
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int pos = 0;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            pos = index;
+            break;
+        }
+        index++;
+    }
+    return pos;
+}
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("");
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
